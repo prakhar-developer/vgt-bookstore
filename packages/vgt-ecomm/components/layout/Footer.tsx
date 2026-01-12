5c7d760e36e899c18af360db322b9c76f210c0b0
prakhar_developer <prakhar_developer@outlook.com>
2026-01-13 02:48:22 +0530

    footer sahi

--- packages/vgt-ecomm/components/layout/Footer.tsx
+++ packages/vgt-ecomm/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import { Mail, Phone, MapPin } from 'lucide-react';
 
 export default function Footer() {
   return (
-    <footer className="bg-[#3D4127] rgb(121 126 23) mt-16">
+    <footer className="bg-vgt-dark text-white mt-16">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg: px-8 py-12">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {/* About */}
