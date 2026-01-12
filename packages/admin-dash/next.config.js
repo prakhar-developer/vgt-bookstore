eee2788146c394eb7ac692c7947bc2df9136417c
prakhar_developer <prakhar_developer@outlook.com>
2026-01-12 22:50:55 +0530

    depjjhh

--- packages/admin-dash/next.config.js
+++ packages/admin-dash/next.config.js
@@ -1,12 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
+  images:  {
+    remotePatterns:  [
       {
-        protocol: 'https',
+        protocol:  'https',
         hostname: 'res.cloudinary.com',
       },
     ],
+  },
+  
+  webpack: (config, { isServer }) => {
+    // Disable cache
+    config.cache = false;
+    
+    // Ignore MongoDB optional dependencies (not needed for basic usage)
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({
+        'aws4': 'commonjs aws4',
+        '@mongodb-js/zstd': 'commonjs @mongodb-js/zstd',
+        'kerberos': 'commonjs kerberos',
+        '@aws-sdk/credential-providers': 'commonjs @aws-sdk/credential-providers',
+        'gcp-metadata': 'commonjs gcp-metadata',
+        'snappy': 'commonjs snappy',
+        'socks': 'commonjs socks',
+        'mongodb-client-encryption': 'commonjs mongodb-client-encryption',
+      });
+    }
+    
+    return config;
   },
 };
 
